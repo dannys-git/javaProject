2f38e179713631c59eac39a08a41a9685158f836
Danish <42232021+dannys-git@users.noreply.github.com>
2022-04-16 17:53:07 +0530

    made some wrong changes, remove a semi-colon

--- Student.java
+++ Student.java
@@ -2,6 +2,6 @@ class Student
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hy my name is Danish. And this is my first JAVA test project");
+		System.out.println("Hy my name is Danish. And this is my first JAVA test project")
 	}
 }
